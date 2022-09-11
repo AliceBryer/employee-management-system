6d4a19d382fdebcddda2ff4b30332388defa1756
Alice Bryer <alicebryer123@gmail.com>
2022-09-11 14:45:38 +0100

    created seed data

---
+++ db/seeds.sql
@@ -0,0 +1,44 @@
+INSERT INTO departments (department_name)
+VALUES ("Cleaners"),
+       ("Chefs"),
+       ("Bar team"),
+       ("Servers"),
+       ("Hosts");
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Head Cleaner", 23000, 1),
+       ("Part-Time-Cleaner", 15000, 1),
+       ("Head chef", 30000, 2),
+       ("Grill chef", 20000, 2),
+       ("Line chef", 21000, 2),
+       ("Porter", 12000, 2)
+       ("Head-bartender", 19000, 3),
+       ("Bartender", 10000, 3),
+       ("Assistant Bartender", 13000, 3),
+       ("Head waitress", 12000, 4),
+       ("Assistant Head waitress" 10000 4),
+       ("Waiter", 8000, 4),
+       ("Host Manager", 10000 5)
+       ("Dining host", 8500, 5),
+       ("outside host", 7000, 5);
+
+       INSERT INTO roles (first_name, last_name, role_id, manager_id)
+VALUES ("Harvey", "Jones", 1, null),
+       ("Debbie", "Smith" 2, 1),
+       ("Mike", "Myers", 3, null), 
+       ("James", "Graham", 4, 3),
+       ("Alison", "Taylow", 5, 3),
+       ("Hamed", "Ali", 6, 3),
+       ("Bobby", "Brown", 7, null),
+       ("Joanne", "Bryer", 8, 7),
+       ("Claire", "West", 9, 7),
+       ("Kim", "Monroe", 10, null),
+       ("Edward", "Jenkins",11, 10),
+       ("Theo", "Ellis", 12, 10),
+       ("Andy", "Beckett", 13, null),
+       ("Pam", "Dawson", 14, 13),
+       ("Dwight", "Wilson", 15, 13),
+      
+
+
+   
