8fbf45d0cc72e880e95daced468856a118e0bfad
Alice Bryer <alicebryer123@gmail.com>
2022-09-16 15:34:31 +0100

    view All employees complete & tested

--- index.js
+++ index.js
@@ -191,3 +191,24 @@ addNewRole = () => {
       });
   });
 };
+
+viewAllEmployees = () => {
+  const sql = `
+SELECT employees.id, 
+employees.first_name, 
+employees.last_name, 
+roles.title, 
+departments.department_name AS department,
+roles.salary, 
+CONCAT (manager.first_name, " ", manager.last_name) AS manager
+FROM employees
+LEFT JOIN roles ON employees.role_id = roles.id
+LEFT JOIN departments ON roles.department_id = departments.id
+LEFT JOIN employees manager ON employees.manager_id = manager.id;`;
+
+  db.query(sql, (err, results) => {
+    if (err) throw err;
+    console.table(results);
+    initialChoices;
+  });
+};
