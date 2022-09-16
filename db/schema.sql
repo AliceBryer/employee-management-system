d7e6e159f40ca3d7879f699f03e455db0e3a5709
Alice Bryer <alicebryer123@gmail.com>
2022-09-16 11:02:14 +0100

    updatd index & schema

--- db/schema.sql
+++ db/schema.sql
@@ -4,31 +4,31 @@ CREATE DATABASE company_db;
 USE company_db;
 
 CREATE TABLE departments (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  movie_name VARCHAR(30) NOT NULL
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  departments_name VARCHAR(30) NOT NULL
 );
 
 CREATE TABLE roles (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    title VARCHAR(30) NOT NULL,
-    salary DECIMAL NOT NULL,
-    department_id INT,
-    FOREIGN KEY (depatment_id)
-    REFERENCES departments(id)
-    ON DELETE SET NULL);
+ id INT AUTO_INCREMENT PRIMARY KEY,
+ title VARCHAR(30) NOT NULL,
+ salary DECIMAL NOT NULL,
+ department_id INT,
+ FOREIGN KEY (department_id)
+ REFERENCES departments(id)
+ ON DELETE SET NULL);
 
 
 
 CREATE TABLE employees (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    first_name VARCHAR(30) NOT NULL,
-    last_name VARCHAR(30) NOT NULL, 
-    role_id INT,
-    FOREIGN KEY (role_id)
-    REFERENCES role(id)
-    ON DELETE SET NULL,
-    manager_id INT,
-    FOREIGN KEY(manager_id)
-    REFERENCES employees(id)
-    ON DELETE SET NULL
+  id INT AUTO_INCREMENT PRIMARY KEY,
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL, 
+  role_id INT,
+  FOREIGN KEY (role_id)
+  REFERENCES role(id)
+  ON DELETE SET NULL,
+  manager_id INT,
+  FOREIGN KEY(manager_id)
+  REFERENCES employees(id)
+  ON DELETE SET NULL
 ;
---
+++ index.js
@@ -0,0 +1,19 @@
+// import packages/dependencies
+
+const inquirer = require("inquirer");
+const cTable = require("console.Table");
+const mysql = require("mysql2");
+
+const db = mysql.createConnection(
+  {
+    host: "localhost",
+    user: "root",
+    password: "Murphy1234.",
+    database: "company_db",
+  },
+  console.log("Success: you are connected")
+);
+
+db.connect(function (err) {
+  if (err) throw err;
+});
